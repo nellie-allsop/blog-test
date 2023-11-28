7371935c1688f54b77c9430cee3c11adfb89ac02
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-28 13:11:33 +0000

    first commit

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,22 @@
+import { getPosts } from "../../lib/posts"
+import Link from "next/link"
+import React from "react"
+
+export default function Page() {
+  const posts = getPosts()
+
+  return (
+    <div>
+      <h2>My posts: </h2>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.slug}>
+              <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
