803503de461958c344d71508588b56350d5729da
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-29 11:20:40 +0000

    add project info

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+	return (
+		<footer>
+			<p>Created by Nellie</p>
+			<Link href="https://github.com/nellie-allsop">Check out my Github</Link>
+		</footer>
+	);
+}
